0c2a500dccc8c27de508e0c6f420dc67a56cfa6f
Jivan <minecraftjivan@gmail.com>
2019-11-26 12:57:56 -0800

    Move card-checking logic to check_if_card_can_be_placed function in functions.py

--- aiplayer.py
+++ aiplayer.py
@@ -1,8 +1,5 @@
-from action_card import ActionCard
-from normal_card import NormalCard
 from player import Player
-import re
-from functions import format_card
+import functions
 
 class AIPlayer(Player):
     def __init__(self, game):
@@ -10,45 +7,9 @@ class AIPlayer(Player):
 
     def play(self):
         for card in self.hand:
-            top_card = self.game.pile[0]
-            
-            # Remove ANSI codes for comparison 
-            top_card_color_text = re.sub(r"\x1b\[[0-9]+m", "", top_card.color)
-            current_card_color_text = re.sub(r"\x1b\[[0-9]+m", "", card.color)
-            
-            # TODO: Change "or" condition to "and" after allowing users to select a color after placing a wild
-            if current_card_color_text == "Wild" or current_card_color_text == self.game.declared_color:
-                print("I can place a {0}!".format(format_card(card)))
+            check = functions.check_if_card_can_be_placed(card, self.game.pile[0], self.game.declared_color)
+            if check:
                 self.game.pile.insert(0, self.hand.pop(self.hand.index(card)))
-                return True
-
-            if top_card_color_text == "Wild":
-                print("I can place a {0}!".format(format_card(card)))
-                self.game.pile.insert(0, self.hand.pop(self.hand.index(card)))
-                return True
-
-            # Change "or" condition to "and" after allowing users to select a color after placing a wild
-            if current_card_color_text == top_card_color_text or current_card_color_text == self.game.declared_color:
-                print("I can place a {0}!".format(format_card(card)))
-                self.game.pile.insert(0, self.hand.pop(self.hand.index(card)))
-                return True
-
-            # TODO: Check if an ActionCard can be placed
-            if type(card) == ActionCard:
-                if type(top_card) == ActionCard:
-                    if card.action == top_card.action:
-                        print("I can place a {0}!".format(format_card(card)))
-                        self.game.pile.insert(0, self.hand.pop(self.hand.index(card)))
-                        return True
-
-            # Check if a NormalCard can be placed
-            if type(card) == NormalCard:
-                if type(top_card) == NormalCard:
-                    if card.number == top_card.number:
-                        print("I can place a {0}!".format(format_card(card)))
-                        self.game.pile.insert(0, self.hand.pop(self.hand.index(card)))
-                        return True
-            print("I can't place a {0} <:(".format(format_card(card)))
-
-
-            
+                print("I can place a {0}!".format(functions.format_card(card)))
+                break
+            print("I can't place a {0} <:(".format(functions.format_card(card)))
--- functions.py
+++ functions.py
@@ -1,6 +1,38 @@
 from normal_card import NormalCard
+from action_card import ActionCard
+import re
+
 def format_card(card):
     if type(card) == NormalCard:
         return "{0} {1}".format(card.color, card.number)
     else:
         return "{0} {1}".format(card.color, card.action)
+
+# declared_color hasn't been implemented yet so its default value is None
+def check_if_card_can_be_placed(card_to_place, card_placed, declared_color=None):
+    card_to_place_color_text = re.sub(r"\x1b\[[0-9]+m", "", card_to_place.color)
+    card_placed_color_text = re.sub(r"\x1b\[[0-9]+m", "", card_placed.color)
+    
+    # TODO: Change "or" condition to "and" after allowing users to select a color after placing a wild
+    if card_to_place_color_text == "Wild" or card_to_place_color_text == declared_color:
+        return True
+
+    if card_placed_color_text == "Wild":
+        return True
+
+    # Change "or" condition to "and" after allowing users to select a color after placing a wild
+    if card_to_place_color_text == card_placed_color_text or card_to_place_color_text == declared_color:
+        return True
+
+    # TODO: Check if an ActionCard can be placed
+    if type(card_to_place) == ActionCard:
+        if type(card_placed) == ActionCard:
+            if card_to_place.action == card_placed.action:
+                return True
+
+    # Check if a NormalCard can be placed
+    if type(card_to_place) == NormalCard:
+        if type(card_placed) == NormalCard:
+            if card_to_place.number == card_placed.number:
+                        return True
+    return False
